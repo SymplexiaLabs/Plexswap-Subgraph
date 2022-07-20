85f46414650532bd0423c31e4d82fa3cf120d498
Symplexia Labs <suporte@symplexia.com.br>
2022-07-20 06:02:40 -0300

    Versions

--- subgraphs/exchange/mappings/utils/index.ts
+++ subgraphs/exchange/mappings/utils/index.ts
@@ -6,7 +6,7 @@ import { ERC20SymbolBytes } from "../../generated/Factory/ERC20SymbolBytes";
 import { Factory as FactoryContract } from "../../generated/templates/Pair/Factory";
 
 export let ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+export let FACTORY_ADDRESS = "0x580B12Fcc6247E7bA7a02324Ea6Aa6604d0BEC7A";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
